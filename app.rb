feb4326d90ce5ab3b659bb87e6b72b5b372284ec
水島翔吾 <info@erss-co.net>
2020-04-08 18:00:23 +0900

    first commit

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
-#require 'charlock_holmes'
 require "csv"
 require "pathname"
 
+mcpa_limit = 700
 
 Pathname.glob(Pathname.getwd.join("**/*")) do |source_path|
   # ファイルじゃないとき（ディレクトリーのとき）はスキップ
@@ -15,85 +15,82 @@ Pathname.glob(Pathname.getwd.join("**/*")) do |source_path|
   csv_datas = CSV.read(source_path.to_s, encoding: "#{encoding}:UTF-8")
 
   head_map = {}
-  key_val_map = {}
-  res = {}
+  res = []
 
   head = csv_datas.shift
 
   head.each_with_index do |title, idx|
-    head_map[idx] = title
+    head_map[title] = idx
   end
 
-  csv_datas.each do |row|
-    unless res[row[1]].nil?
-      row.each_with_index do |val, idx|
-        if head_map[idx] == "Creative" ||
-          head_map[idx] == "通貨" ||
-          head_map[idx] == "Date" ||
-          head_map[idx] == "Margin(Agency)" ||
-          head_map[idx] == "原価" ||
-          head_map[idx] == "CPA消化金額" ||
-          head_map[idx] == "CTR" ||
-          head_map[idx] == "eCPC"
-          next
-        end
-        res[row[1]][head_map[idx]] = res[row[1]][head_map[idx]] + val.to_f
-        key_val_map[head_map[idx]] = key_val_map[head_map[idx]] + val.to_f
+  p head_map
+
+
+  csv_datas.each.with_index(1) do |row, idx|
+
+    if row[head_map["cv click"]].to_i > 0
+      # mCVRが発生している場合
+      # mcpa_limit/mCPA x CPCをbidpriceにする
+      p "=== mCVR > 0 ==="
+      p "cpc is #{row[head_map["cpc"]]}"
+      p "bidprice is #{row[head_map["bidprice"]]}"
+      p "mCPA is #{row[head_map["cpa/cpi"]]}"
+      row[head_map["bidprice"]] = (row[head_map["bidprice"]].to_f * (mcpa_limit/(row[head_map["cpa/cpi"]].to_f)) ).round
+      row[head_map["lowest bid price"]] =  row[head_map["bidprice"]] - 1
+      if mcpa_limit > row[head_map["cpa/cpi"]].to_i
+        #上限mCPAより安く収まっているもの -> 増額
+        p "bidprice increased to #{row[head_map["bidprice"]]}"
+      else
+        # 上限mCPAを超えているもの -> 減額
+        p "bidprice reduced to #{row[head_map["bidprice"]]}"
       end
     else
-      res[row[1]] = {}
-      row.each_with_index do |val, idx|
-        if head_map[idx] == "Creative" ||
-          head_map[idx] == "通貨" ||
-          head_map[idx] == "Date" ||
-          head_map[idx] == "Margin(Agency)" ||
-          head_map[idx] == "原価" ||
-          head_map[idx] == "CPA消化金額" ||
-          head_map[idx] == "CTR" ||
-          head_map[idx] == "eCPC"
-          next
-        end
-        res[row[1]][head_map[idx]] = val.to_f
-        key_val_map[head_map[idx]] = val.to_f
+      # mCVRがまだ発生していないもの
+      if row[head_map["total cost"]].to_i > mcpa_limit
+        # cpa上限より消化が多いもの
+        p "=== total cost > mcpa_limit ==="
+        p "cpc is #{row[head_map["cpc"]]}"
+        p "bidprice is #{row[head_map["bidprice"]]}"
+        p "total cost is #{row[head_map["total cost"]]}"
+        row[head_map["bidprice"]] = ( row[head_map["bidprice"]].to_f * (mcpa_limit/(row[head_map["total cost"]].to_f)) ).round
+        row[head_map["lowest bid price"]] =  row[head_map["bidprice"]] - 1
+        p "bidprice reduced to #{row[head_map["bidprice"]]}"
+      elsif row[head_map["total cost"]].to_i == 0
+        #消化がまだないもの
+        p "=== cost 0 ==="
+        p "cpc is #{row[head_map["cpc"]]}"
+        p "bidprice is #{row[head_map["bidprice"]]}"
+        p "total cost is #{row[head_map["total cost"]]}"
+        row[head_map["bidprice"]] = row[head_map["bidprice"]].to_i + 1
+        row[head_map["lowest bid price"]] =  row[head_map["bidprice"]] - 1
+        p "bidprice increased to #{row[head_map["bidprice"]]}"
+      else
+        #上記いずれも当てはまらないもの
+        #cpcより1円低い金額に設定
+        p "=== few cost ==="
+        p "cpc is #{row[head_map["cpc"]]}"
+        p "bidprice is #{row[head_map["bidprice"]]}"
+        p "total cost is #{row[head_map["total cost"]]}"
+        row[head_map["bidprice"]] = row[head_map["cpc"]].to_i - 1
+        row[head_map["lowest bid price"]] = row[head_map["bidprice"]] - 1
+        p "bidprice reduced to #{row[head_map["bidprice"]]}"
       end
     end
-  end
-  remove_keys = []
-  mcv_keys = []
-  key_val_map.each do |key, val|
-    if val.to_i == 0
-      remove_keys.push(key)
-    else
-      if key =~ /mCV/
-        mcv_keys.push key
-      end
-    end
+
+    res.push(row)
   end
   #p mcv_keys
-  head = ["Title","Imp","消化","Click","CPC", "CTR", "mCV", "mCPA"]
+  head = head_map.keys
   CSV.open(output_file,'w') do |body|
     body << head
-    res.each do |title, row|
-      p row
-      output_row = []
-      output_row.push(title)
-      output_row.push(row["Imp"])
-      output_row.push(row["消化"])
-      output_row.push(row["Click"])
-      output_row.push(row["消化"]/row["Click"])
-      output_row.push((row["Click"]/row["Imp"])*100)
-      mcv_count = nil
-      mcv_keys.each do |key|
-        if key.include? "CV (Click)"
-          mcv_count = row[key]
-        end
+    res.each do |row|
+      if row[head_map["bidprice"]] < 0
+        row[head_map["bidprice"]] = 1
       end
-      unless mcv_count.nil?
-        output_row.push(mcv_count)
+      if row[head_map["lowest bid price"]] < 0
+        row[head_map["lowest bid price"]] = 1
       end
-      output_row.push(row["消化"]/mcv_count)
-      #p output_row
-      body << output_row
+      body << row
     end
   end
 end
